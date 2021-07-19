89639a76d2357d5782799d00de20fceada1cf0ed
Nataliya Bondarenko <nataliyabondarenko@MacBook-Pro-Nataliya.local>
2021-07-19 17:29:43 +0300

    1729

--- src/services/currencyAPI.js
+++ src/services/currencyAPI.js
@@ -1,37 +1,40 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 const baseURL = "https://cmc-api-proxy-lmvbixv5b-spean92.vercel.app";
 
+const getCurrencies = async () => {
+  try {
+    const responsePrices = await axios.get(
+      `${baseURL}/cryptocurrency/quotes/latest?symbol=BTC,DOGE,ETH`
+    );
+    const currPrices = Object.values(responsePrices.data.data);
 
-const getCurrencies = async () =>{
-const responsePrices = await axios.get(`${baseURL}/cryptocurrency/quotes/latest?symbol=BTC,DOGE,ETH`)
-const currPrices = (Object.values(responsePrices.data.data));
+    const responseImg = await axios.get(
+      `${baseURL}/cryptocurrency/info?symbol=BTC,DOGE,ETH&aux=logo`
+    );
 
-const responseImg = await axios.get(`${baseURL}/cryptocurrency/info?symbol=BTC,DOGE,ETH&aux=logo`)
+    const currImg = Object.values(responseImg.data.data);
 
-const currImg = (Object.values(responseImg.data.data));
+    const mergeByProperty = (target, source, prop) => {
+      source.forEach((sourceElement) => {
+        let targetElement = target.find((targetElement) => {
+          return sourceElement[prop] === targetElement[prop];
+        });
+        targetElement
+          ? Object.assign(targetElement, sourceElement)
+          : target.push(sourceElement);
+      });
+    };
 
+    mergeByProperty(currPrices, currImg, "symbol");
 
-const mergeByProperty = (target, source, prop) => {
-  source.forEach(sourceElement => {
-    let targetElement = target.find(targetElement => {
-      return sourceElement[prop] === targetElement[prop];
-    })
-    targetElement ? Object.assign(targetElement, sourceElement) : target.push(sourceElement);
-  })
-}
-
-mergeByProperty(currPrices, currImg, 'symbol');
-
-return currPrices
-}
-
-
-
+    return currPrices;
+  } catch (error) {
+    console.log("error", { error });
+    throw error;
+  }
+};
 
 export default {
-    getCurrencies 
-}
-
-
+  getCurrencies,
+};
